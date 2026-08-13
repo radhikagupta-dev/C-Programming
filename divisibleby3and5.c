0edcca06b7149a6b2d6104576096c0459da9301b
radhikagupta-dev <radhika.gupta5988@gmail.com>
2026-08-13 23:16:13 +0530

    Add files via upload

---
+++ divisibleby3and5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x;
+    printf("Enter a number:");
+    scanf("%d",&x);
+    if(x%5==0 && x%3==0){
+        printf("Number is divisible by 3 and 5");
+        }    
+    else{
+        printf("Number is not divisible by 3 and 5");
+    }
+    return 0;
+}
